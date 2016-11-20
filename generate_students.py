31fbcbf3b07ef8ee7c29d23bfeafc3161a5959db
Ambika <aacharya@stanford.edu>
2016-11-20 14:12:20 -0800

    added student model

---
+++ generate_students.py
@@ -0,0 +1,6 @@
+import student
+def one_student():
+	s = Student("CT1",[1.45,3])
+	print s.ct
+
+one_student()
---
+++ student.py
@@ -0,0 +1,5 @@
+class Student:
+	def __init__(self,census_tract,closest):
+		self.ct = census_tract
+		self.closest_schools = closest
+		self.rankings = None
